ad59aba70ad51240b6d18fa9d065bc497010ae6a
Dedei-AB <andreiabners@gmail.com>
2025-12-09 11:26:39 -0300

    Revert "imports arrumados"
    This reverts commit e9064660104f87c239f63a45ef7e3998d04a7c72.

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Perfil from "./pages/Perfil";
-import Cadastro from "./pages/CriarConta/Cadastro";
-import Login from "./pages/CriarConta/Login";
-import CriarQuizz from "./pages/CriacaoQuizze/CriarQuizz";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
+import CriarQuizz from "./pages/CriarQuizz";
 import Quizzes from "./pages/Quizzes";
 import "../src/Global.css";
 import NavBar from "./components/NavBar";
-import { LoginProvider } from "./context/LoginContext";
-import TelaPergunta from "./pages/QuizzJogo/TelaPergunta";
+import { LoginProvider } from "./LoginContext";
+import TelaPergunta from "./pages/TelaPergunta";
 
 function ComNavBar() {
   return (
