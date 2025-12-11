139fd822b5f50debb537cf28e0dc4168e5021031
emilyshack <163414103+emilyshack@users.noreply.github.com>
2025-12-10 23:42:54 -0300

    correção de bugs

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -1,14 +1,3 @@
-import NavBar from "../components/NavBar";
-import styles from "./Home.module.css";
-
-function Quizzes() {
-  return (
-    <div className={`${styles["tela-principal"]}`}>
-      <h1>Página de Quizzes</h1>
-    </div>
-  );
-}
-
 export default Quizzes;
 import { Plus } from "lucide-react";
 import home from "./Home.module.css";     
