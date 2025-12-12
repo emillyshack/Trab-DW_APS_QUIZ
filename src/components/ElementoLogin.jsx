51ee501530bd1a4165ca0b2f24db45a4325207a0
Dedei-AB <andreiabners@gmail.com>
2025-12-12 11:38:55 -0300

    tela login pronta

--- src/components/ElementoLogin.jsx
+++ src/components/ElementoLogin.jsx
@@ -1,23 +1,27 @@
 import styles from "./ElementoLogin.module.css";
 import logo from "../assets/images/Quizzy_logo.png";
+import pessoa from "../assets/images/pessoa-joinha-removebg-preview.png";
 import { useContext } from "react";
 import { GeralContexto } from "../context/GeralContext";
-import Pergunta from "../components/Pergunta";
+import Quizz from "./Quizz";
 
 function ElementoLogin() {
   const { quizzes } = useContext(GeralContexto);
   return (
     <div className={`${styles["tela-principal"]}`}>
-      <img
-        src={logo}
-        alt="Logo do Site"
-        className={styles.logo}
-        title="Quizzy"
-      />
-      <div className={styles.quizzes}>
-        {quizzes.map((p, index) => (
-          <Pergunta key={index} pergunta={p.titulo} />
-        ))}
+      <img src={pessoa} alt="Pessoa joinha" className={styles.pessoa} />
+      <div className={styles.quizz}>
+        <img
+          src={logo}
+          alt="Logo do Site"
+          className={styles.logo}
+          title="Quizzy"
+        />
+        <div className={styles.quizzes}>
+          {quizzes.map((p, index) => (
+            <Quizz key={index} titulo={p.titulo} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -3,6 +3,7 @@ import styles from "./Quizz.module.css";
 function Quizz({ titulo }) {
   return (
     <div className={`${styles.container} doodle-border`}>
+      <h2>Quizz:</h2>
       <p className={titulo}>{titulo}</p>
     </div>
   );
